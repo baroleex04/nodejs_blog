fe29cf16b242dfac5551a139be5be2a2cf3fa97e
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-03-06 20:42:22 +0700

    Add cards of courses on the site page

--- src/index.js
+++ src/index.js
@@ -33,11 +33,11 @@ app.set(
     'view engine',
     'hbs',
 );
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources' ,'views'));
 
 // Routes init
 route(app);
 
-                  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
 });
