5408f9876d6c71427f97546ea92db99bde748463
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-03-13 09:47:11 +0700

    Finish select all

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -9,46 +9,61 @@ class CourseController {
 
     // [GET] /courses/:slug
     show(req, res, next) {
-        Course.findOne({slug: req.params.slug})
-            .then(course => 
-                res.render('courses/show', { course: mongooseToObject(course)
-            }))
+        Course.findOne({ slug: req.params.slug })
+            .then(course =>
+                res.render('courses/show', {
+                    course: mongooseToObject(course)
+                }))
             .catch(next);
     }
 
     // [GET] /courses/:id/edit
     edit(req, res, next) {
         Course.findById(req.params.id)
-            .then(course => 
-                res.render('courses/edit', { course: mongooseToObject(course)
-            }))
+            .then(course =>
+                res.render('courses/edit', {
+                    course: mongooseToObject(course)
+                }))
             .catch(next)
     }
 
     // [PUT] /courses/:id
     update(req, res, next) {
-        Course.updateOne({_id: req.params.id}, req.body)
+        Course.updateOne({ _id: req.params.id }, req.body)
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next)
     }
 
     // [PATCH] /courses/:id/restore
     restore(req, res, next) {
-        Course.restore({_id: req.params.id})
+        Course.restore({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next)
     }
 
+    // [POST] /courses/handle-form-action
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds} })
+                    .then(() => res.redirect('back'))
+                    .catch(next)
+                break;
+            default:
+                res.json({ message: "Action is invalid" })
+        }
+    }
+
     // [DELETE] /courses/:id
     destroy(req, res, next) {
-        Course.delete({_id: req.params.id})
+        Course.delete({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next)
     }
 
     // [DELETE] /courses/:id/force
     forceDestroy(req, res, next) {
-        Course.deleteOne({_id: req.params.id})
+        Course.deleteOne({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next)
     }
@@ -60,7 +75,7 @@ class CourseController {
         await course.save()
             .then(res.redirect('/me/stored/courses'))
             .catch(error => {
-                
+
             })
     }
 }
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,7 @@ const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/handle-form-action', courseController.handleFormActions);
 router.get('/:id/edit', courseController.edit);
 router.patch('/:id/restore', courseController.restore);
 router.put('/:id', courseController.update);
